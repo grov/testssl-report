05ae65216c073bd3eb0b789995cf5138bc7c4410
Johnny John Johnnson <yoann.audon@gmail.com>
2018-12-10 11:17:32 +0100

    Update cloneRunAndAggregate.sh

--- cloneRunAndAggregate.sh
+++ cloneRunAndAggregate.sh
@@ -2,8 +2,8 @@ git clone https://github.com/drwetter/testssl.sh.git
 mkdir -p results
 echo '"Application","id","fqdn/ip","port","severity","finding"' > results/fails.csv
 while read url; do
-    echo "./testssl.sh/testssl.sh -p -- openssl-timeout 5 --csvfile=results/$url $url" >> results/run.log
-    ./testssl.sh/testssl.sh --csvfile=results/$url $url
+    echo "./testssl.sh/testssl.sh -p --openssl-timeout 5 --csvfile=results/$url $url" >> results/run.log
+    ./testssl.sh/testssl.sh -p --openssl-timeout 5 --csvfile=results/$url $url
     cd results
     ../aggregateFails.sh $url >> fails.csv
     echo "../aggregateFails.sh $url >> fails.csv" >> run.log
